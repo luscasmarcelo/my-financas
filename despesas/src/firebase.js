6e507b1d80b44d5dd17e5ce5f67c745bffd6f0b6
Lucas Marcelo Sampaio Santos <149593118+luscasmarcelo@users.noreply.github.com>
2024-10-10 13:07:59 +0000

    Reorganiza credenciais para variáveis de ambiente

--- despesas/src/firebase.js
+++ despesas/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAvFFuy-qwlykCQCB_0t786l3nJB6Bd6GI",
-    authDomain: "myfinancasfb.firebaseapp.com",
-    projectId: "myfinancasfb",
-    storageBucket: "myfinancasfb.appspot.com",
-    messagingSenderId: "281768201924",
-    appId: "1:281768201924:web:e1cae87a99f2b6566f4391"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
