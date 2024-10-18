3d70d364ddd116d6d39d9b941ad463d75d43a41e
Lucas Marcelo Sampaio Santos <149593118+luscasmarcelo@users.noreply.github.com>
2024-10-18 21:15:14 +0000

    adicionando funcionalidade para o botão editar despesa

--- despesas/src/components/GerenciamentoDespesas.js
+++ despesas/src/components/GerenciamentoDespesas.js
@@ -7,6 +7,7 @@ const GerenciamentoDespesas = ({ usuarioId }) => {
   const [despesas, setDespesas] = useState([]);
   const [mesSelecionado, setMesSelecionado] = useState('');
   const [novaDespesa, setNovaDespesa] = useState({ categoria: '', valor: '', descricao: '' });
+  const [despesaEdicao, setDespesaEdicao] = useState(null);
 
   const fetchDespesas = async () => {
     try {
@@ -36,13 +37,19 @@ const GerenciamentoDespesas = ({ usuarioId }) => {
     }
   };
 
-  const handleUpdateDespesa = async (id, despesaAtualizada) => {
+  const startEditing = (despesa) => {
+    setDespesaEdicao(despesa);
+  }
+
+  const handleSaveEdit = async ()=> {
     try {
-      const response = await updateDespesa(id, despesaAtualizada);
-      setDespesas(despesas.map((despesa) => (despesa._id === id ? response.data : despesa)));
-      alert("Despesa atualizada com sucesso!")
+      const response = await updateDespesa(despesaEdicao._id, despesaEdicao);
+      setDespesas(despesas.map((despesa) => (despesa._id === despesaEdicao._id ? response.data : despesa)));
+      setDespesaEdicao(null);
+      alert("Despesa atualizada com sucesso!");
     } catch (error) {
-      console.error('Erro ao atualizar despesa:', error);
+      alert("Erro ao atualizar despesa.");
+      console.error(error);
     }
   };
 
@@ -102,6 +109,18 @@ const GerenciamentoDespesas = ({ usuarioId }) => {
         <button onClick={handleCreateDespesa}>Adicionar Despesa</button>
       </div>
 
+      {despesaEdicao && (
+        <div>
+          <h3>Editar Despesa</h3>
+          <input type="text" placeholder='Categoria' value={despesaEdicao.categoria} onChange={(e) => setDespesaEdicao({ ...despesaEdicao, categoria: e.target.value})} />
+          <input type='number' placeholder='Valor' value={despesaEdicao.valor} onChange={(e) => setDespesaEdicao({ ...despesaEdicao, valor: e.target.value })} />
+          <input type='text' placeholder='Descrição' value={despesaEdicao.descricao} onChange={(e) => setDespesaEdicao({ ...despesaEdicao, descricao: e.target.value })} />
+
+          <button onClick={handleSaveEdit}>Salvar Alterações</button>
+          <button onClick={() => setDespesaEdicao(null)}>Cancelar</button>
+        </div>
+      )}
+
       {/* Lista de Despesas */}
       <div>
         <h3>Despesas</h3>
@@ -110,7 +129,7 @@ const GerenciamentoDespesas = ({ usuarioId }) => {
             <p>Categoria: {despesa.categoria}</p>
             <p>Valor: {despesa.valor}</p>
             <p>Descrição: {despesa.descricao}</p>
-            <button onClick={() => handleUpdateDespesa(despesa._id, { ...despesa, valor: despesa.valor + 10 })}>Editar</button>
+            <button onClick={() => startEditing(despesa)}>Editar</button>
             <button onClick={() => handleDeleteDespesa(despesa._id)}>Deletar</button>
           </div>
         ))}
